4223c3798cb4d83d041a26ec3161e22b150ce6b5
Tejaswini074 <tejaswinis532@gmail.com>
2026-06-23 17:58:24 +0530

    Backend setup completed

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('DevPulse API Running');
+});
 app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    message: 'DevPulse Backend Running'
-  });
+    res.status(200).json({
+        status: 'OK',
+        message: 'DevPulse Backend Running'
+    });
 });
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
