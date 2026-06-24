ea2f0fbb09ac5db7bf556307474de6ca4a48da38
Tejaswini074 <tejaswinis532@gmail.com>
2026-06-24 11:07:53 +0530

    Implement JWT authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcryptjs');
 const db = require('../config/db');
-
+const jwt = require('jsonwebtoken');
 exports.register = async (req, res) => {
   try {
-
     const { name, email, password, role } = req.body;
 
     if (!name || !email || !password) {
@@ -13,30 +12,65 @@ exports.register = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const sql = `INSERT INTO users (name,email,password,role) VALUES (?,?,?,?) `;
 
-    db.query(
-      sql,
-      [name, email, hashedPassword, role || 'Developer'
-      ],
-      (err, result) => {
-        if (err) {
-          return res.status(500).json(err);
-        }
-        res.status(201).json({
-          message: 'User Registered'
-        });
-
+    db.query(sql, [name, email, hashedPassword, role || 'Developer'], (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
       }
-    );
+      res.status(201).json({
+        message: 'User Registered'
+      });
+    });
   } catch (error) {
     res.status(500).json(error);
   }
 };
 
-exports.login = async (req, res) => {
-  res.status(200).json({
-    message: 'Login API coming soon'
-  });
+exports.login = (req, res) => {
+
+  const { email, password } = req.body;
+
+  const sql = 'SELECT * FROM users WHERE email = ?';
+
+  db.query(sql, [email], async (err, results) => {
+
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (results.length === 0) {
+      return res.status(400).json({
+        message: 'Invalid Email'
+      });
+    }
+
+    const user = results[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: 'Invalid Password'
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+
+      process.env.JWT_SECRET, { expiresIn: '7d' }
+    );
+
+    res.status(200).json({
+      message: 'Login Successful', token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  }
+  );
 };
